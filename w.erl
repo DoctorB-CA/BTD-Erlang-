96d25d58a15ff17ad8e2a4df3b577db3aa2fcef9
DoctorB <150272448+DoctorB-CA@users.noreply.github.com>
2025-08-06 19:14:51 +0300

    Update w.erl

--- w.erl
+++ w.erl
@@ -3,12 +3,14 @@
 
 -include_lib("wx/include/wx.hrl").
 
--define(BUTTON_WIDTH, 80).
--define(BUTTON_HEIGHT, 80).
 -define(WINDOW_WIDTH, 1000).
 -define(WINDOW_HEIGHT, 1000).
--define(MAP_WIDTH, 700).
--define(MAP_HEIGHT, 600).
+-define(BUTTON_WIDTH, 80).
+-define(BUTTON_HEIGHT, 80).
+-define(CANVAS_WIDTH, 700).
+-define(CANVAS_HEIGHT, 600).
+-define(BALLOON_WIDTH, 40).
+-define(BALLOON_HEIGHT, 40).
 
 start() ->
     spawn(fun ?MODULE:init/0).
@@ -16,103 +18,134 @@ start() ->
 init() ->
     wx:new(),
     wxImage:initStandardHandlers(),
-    MainPID = self(),
-    Frame = wxFrame:new(wx:null(), ?wxID_ANY, "Monkey Menu"),
-    Panel = wxPanel:new(Frame),
-    wxPanel:setBackgroundColour(Panel, {255, 255, 255}),
 
-    FolderPath = "/home/csestudent/Desktop/bar/",
-    DartMonkeyPath = FolderPath ++ "mmmmmmonkey.png",
-    FireMonkeyPath = FolderPath ++ "fire_monkey.png",
-    MapPath = FolderPath ++ "map.png",
+    Frame = wxFrame:new(wx:null(), ?wxID_ANY, "Monkey Game"),
+    BackgroundPanel = wxPanel:new(Frame),
+    wxPanel:setBackgroundColour(BackgroundPanel, {255, 255, 255}),
 
-    DartMonkeyImage = wxImage:new(DartMonkeyPath),
-    FireMonkeyImage = wxImage:new(FireMonkeyPath),
-    MapImage = wxImage:new(MapPath),
+    %% --- 1. Load Bitmaps ---
+    DartBitmap = wxBitmap:new(wxImage:scale(wxImage:new("mmmmmmonkey.png"), ?BUTTON_WIDTH, ?BUTTON_HEIGHT)),
+    FireBitmap = wxBitmap:new(wxImage:scale(wxImage:new("fire_monkey.png"), ?BUTTON_WIDTH, ?BUTTON_HEIGHT)),
+    MapBitmap = wxBitmap:new(wxImage:scale(wxImage:new("map.png"), ?CANVAS_WIDTH, ?CANVAS_HEIGHT)),
+    RedBalloonBitmap = wxBitmap:new(wxImage:scale(wxImage:new("red_balloon.png"), ?BALLOON_WIDTH, ?BALLOON_HEIGHT)),
 
-    DartMonkeyScaled = wxImage:scale(DartMonkeyImage, ?BUTTON_WIDTH, ?BUTTON_HEIGHT),
-    FireMonkeyScaled = wxImage:scale(FireMonkeyImage, ?BUTTON_WIDTH, ?BUTTON_HEIGHT),
-    MapScaled = wxImage:scale(MapImage, ?MAP_WIDTH, ?MAP_HEIGHT),
+    %% --- 2. Create Widgets (NO wxTimer) ---
+    DartButton = wxStaticBitmap:new(BackgroundPanel, ?wxID_ANY, DartBitmap),
+    FireButton = wxStaticBitmap:new(BackgroundPanel, ?wxID_ANY, FireBitmap),
+    CanvasPanel = wxPanel:new(BackgroundPanel, [{style, ?wxBORDER_SUNKEN}, {size, {?CANVAS_WIDTH, ?CANVAS_HEIGHT}}]),
+    StartWaveButton = wxButton:new(BackgroundPanel, ?wxID_ANY, [{label, "Start Wave"}]),
 
-    DartMonkeyBitmap = wxBitmap:new(DartMonkeyScaled),
-    FireMonkeyBitmap = wxBitmap:new(FireMonkeyScaled),
-    MapBitmap = wxBitmap:new(MapScaled),
-
-    DartMonkeyWidget = wxStaticBitmap:new(Panel, ?wxID_ANY, DartMonkeyBitmap),
-    FireMonkeyWidget = wxStaticBitmap:new(Panel, ?wxID_ANY, FireMonkeyBitmap),
-    MapWidget = wxStaticBitmap:new(Panel, ?wxID_ANY, MapBitmap),
-
+    %% --- 3. Layout ---
+    TopRowSizer = wxBoxSizer:new(?wxHORIZONTAL),
+    wxSizer:add(TopRowSizer, DartButton, [{flag, ?wxLEFT}, {border, 10}]),
+    wxSizer:add(TopRowSizer, FireButton, [{flag, ?wxLEFT}, {border, 10}]),
+    wxSizer:addStretchSpacer(TopRowSizer),
+    wxSizer:add(TopRowSizer, StartWaveButton, [{flag, ?wxRIGHT bor ?wxALIGN_CENTER_VERTICAL}, {border, 10}]),
     PanelSizer = wxBoxSizer:new(?wxVERTICAL),
-    wxSizer:add(PanelSizer, DartMonkeyWidget, [{flag, ?wxALIGN_CENTER_HORIZONTAL bor ?wxTOP bor ?wxBOTTOM}, {border, 10}]),
-    wxSizer:add(PanelSizer, FireMonkeyWidget, [{flag, ?wxALIGN_CENTER_HORIZONTAL bor ?wxBOTTOM}, {border, 10}]),
-    wxSizer:add(PanelSizer, MapWidget, [{flag, ?wxALIGN_CENTER_HORIZONTAL bor ?wxBOTTOM}, {border, 10}]),
-    wxPanel:setSizer(Panel, PanelSizer),
-
+    wxSizer:add(PanelSizer, TopRowSizer, [{flag, ?wxEXPAND bor ?wxTOP bor ?wxBOTTOM}, {border, 10}]),
+    wxSizer:add(PanelSizer, CanvasPanel, [{flag, ?wxALIGN_CENTER_HORIZONTAL bor ?wxBOTTOM}, {border, 10}]),
+    wxPanel:setSizer(BackgroundPanel, PanelSizer),
     MainSizer = wxBoxSizer:new(?wxVERTICAL),
-    wxSizer:add(MainSizer, Panel, [{proportion, 1}, {flag, ?wxEXPAND}]),
+    wxSizer:add(MainSizer, BackgroundPanel, [{proportion, 1}, {flag, ?wxEXPAND}]),
     wxFrame:setSizer(Frame, MainSizer),
     wxFrame:setSize(Frame, {?WINDOW_WIDTH, ?WINDOW_HEIGHT}),
 
+    %% --- 4. Event Handling ---
     wxFrame:connect(Frame, close_window),
-    wxStaticBitmap:connect(DartMonkeyWidget, left_down),
-    wxStaticBitmap:connect(FireMonkeyWidget, left_down),
+    wxStaticBitmap:connect(DartButton, left_down),
+    wxStaticBitmap:connect(FireButton, left_down),
+    wxPanel:connect(CanvasPanel, left_down),
+    wxPanel:connect(CanvasPanel, paint),
+    wxButton:connect(StartWaveButton, command_button_clicked),
 
+    %% --- 5. Start the Loop ---
     wxFrame:show(Frame),
-    InitialState = #{x => undefined, y => undefined},
-    loop(Frame, DartMonkeyWidget, FireMonkeyWidget, MainPID, InitialState).
+    InitialState = #{
+      dart_bitmap => DartBitmap,
+      fire_bitmap => FireBitmap,
+      map_bitmap => MapBitmap,
+      red_balloon_bitmap => RedBalloonBitmap,
+      ticks => 0,
+      current_brush => unselected,
+      stamps => [],
+      balloons => []
+     },
+    loop(Frame, DartButton, FireButton, CanvasPanel, StartWaveButton, InitialState).
 
-custom_input_dialog(ParentPID, ParentFrame, Prompt) ->
-    DialogFrame = wxFrame:new(ParentFrame, ?wxID_ANY, "Input Required"),
-    DialogPanel = wxPanel:new(DialogFrame),
-    Text = wxStaticText:new(DialogPanel, ?wxID_ANY, Prompt),
-    InputBox = wxTextCtrl:new(DialogPanel, ?wxID_ANY, [{value, "0"}]),
-    OK_Button = wxButton:new(DialogPanel, ?wxID_ANY, [{label, "OK"}]),
 
-    Sizer = wxBoxSizer:new(?wxVERTICAL),
-    wxSizer:add(Sizer, Text, [{flag, ?wxALL}, {border, 5}]),
-    wxSizer:add(Sizer, InputBox, [{flag, ?wxEXPAND bor ?wxALL}, {border, 5}]),
-    wxSizer:add(Sizer, OK_Button, [{flag, ?wxALIGN_CENTER bor ?wxALL}, {border, 5}]),
-    DialogPanel:setSizer(Sizer),
-    wxFrame:fit(DialogFrame),
-
-    wxButton:connect(OK_Button, command_button_clicked),
-    wxFrame:connect(DialogFrame, close_window),
-
-    wxFrame:show(DialogFrame),
-    dialog_loop(DialogFrame, OK_Button, InputBox, ParentPID).
-
-dialog_loop(DialogFrame, OK_Button, InputBox, ParentPID) ->
-    receive
-        #'wx'{obj=OK_Button, event=#'wxCommand'{type=command_button_clicked}} ->
-            Value = wxTextCtrl:getValue(InputBox),
-            ParentPID ! {input_result, Value},
-            wxFrame:destroy(DialogFrame);
-        #'wx'{obj=DialogFrame, event=#'wxClose'{}} ->
-            wxFrame:destroy(DialogFrame)
-    end.
-
-loop(Frame, DartMonkeyWidget, FireMonkeyWidget, MainPID, State) ->
+loop(Frame, DartButton, FireButton, CanvasPanel, StartWaveButton, State) ->
     receive
         #'wx'{obj=Frame, event=#'wxClose'{}} ->
             io:format("Closing window.~n");
 
-        #'wx'{obj=DartMonkeyWidget, event=#'wxMouse'{type=left_down}} ->
-            io:format("Dart monkey clicked!~n"),
-            spawn(fun() -> custom_input_dialog(MainPID, Frame, "Enter X Coordinate:") end),
-            loop(Frame, DartMonkeyWidget, FireMonkeyWidget, MainPID, State);
+        #'wx'{obj=DartButton, event=#'wxMouse'{type=left_down}} ->
+            NewState = State#{current_brush => dart_brush},
+            loop(Frame, DartButton, FireButton, CanvasPanel, StartWaveButton, NewState);
 
-        #'wx'{obj=FireMonkeyWidget, event=#'wxMouse'{type=left_down}} ->
-            io:format("Fire monkey clicked!~n"),
-            spawn(fun() -> custom_input_dialog(MainPID, Frame, "Enter X Coordinate:") end),
-            loop(Frame, DartMonkeyWidget, FireMonkeyWidget, MainPID, State);
+        #'wx'{obj=FireButton, event=#'wxMouse'{type=left_down}} ->
+            NewState = State#{current_brush => fire_brush},
+            loop(Frame, DartButton, FireButton, CanvasPanel, StartWaveButton, NewState);
 
-        {input_result, ValueX} ->
-            io:format("Value for X has been saved: ~s~n", [ValueX]),
-            NewState = State#{x => ValueX},
-            loop(Frame, DartMonkeyWidget, FireMonkeyWidget, MainPID, NewState);
+        % If "Start Wave" is clicked, add a balloon and START THE ERLANG TIMER
+        #'wx'{obj=StartWaveButton, event=#'wxCommand'{type=command_button_clicked}} ->
+            io:format("Starting wave... Placing a red balloon.~n"),
+            NewBalloons = [{red, 50, 50}],
+            NewState = State#{balloons => NewBalloons, ticks => 0},
+            % Send the first 'tick' message to ourselves after 1000ms
+            timer:send_after(1000, self(), {tick}),
+            wxWindow:refresh(CanvasPanel),
+            loop(Frame, DartButton, FireButton, CanvasPanel, StartWaveButton, NewState);
+
+        % NEW: This clause handles the message from the standard Erlang timer
+        {tick} ->
+            #{balloons := Balloons, ticks := Ticks} = State,
+            if
+                Ticks < 3 ->
+                    io:format("Tick ~p... moving balloon.~n", [Ticks + 1]),
+                    [{Color, X, Y}] = Balloons,
+                    NewBalloons = [{Color, X + 100, Y}],
+                    NewState = State#{balloons => NewBalloons, ticks => Ticks + 1},
+                    wxWindow:refresh(CanvasPanel),
+                    % Send the NEXT 'tick' message to ourselves
+                    timer:send_after(1000, self(), {tick}),
+                    loop(Frame, DartButton, FireButton, CanvasPanel, StartWaveButton, NewState);
+                true -> % Ticks >= 3, animation is over
+                    io:format("Animation finished.~n"),
+                    loop(Frame, DartButton, FireButton, CanvasPanel, StartWaveButton, State)
+            end;
+
+        #'wx'{obj=CanvasPanel, event=#'wxMouse'{type=left_down, x=X, y=Y}} ->
+            #{current_brush := BrushType, stamps := Stamps} = State,
+            case BrushType of
+                unselected ->
+                    io:format("Please select a brush first.~n"),
+                    loop(Frame, DartButton, FireButton, CanvasPanel, StartWaveButton, State);
+                _ ->
+                    StampX = X - (?BUTTON_WIDTH div 2),
+                    StampY = Y - (?BUTTON_HEIGHT div 2),
+                    NewStamps = [{BrushType, StampX, StampY} | Stamps],
+                    NewState = State#{stamps => NewStamps, current_brush => unselected},
+                    wxWindow:refresh(CanvasPanel),
+                    loop(Frame, DartButton, FireButton, CanvasPanel, StartWaveButton, NewState)
+            end;
+
+        #'wx'{obj=CanvasPanel, event=#'wxPaint'{}} ->
+            #{stamps := Stamps, balloons := Balloons, dart_bitmap := DartBitmap,
+              fire_bitmap := FireBitmap, map_bitmap := MapBitmap,
+              red_balloon_bitmap := RedBalloonBitmap} = State,
+            DC = wxBufferedPaintDC:new(CanvasPanel),
+            wxDC:drawBitmap(DC, MapBitmap, {0, 0}),
+            lists:foreach(fun({BrushType, PX, PY}) ->
+                                  Bitmap = case BrushType of dart_brush -> DartBitmap; fire_brush -> FireBitmap end,
+                                  wxDC:drawBitmap(DC, Bitmap, {PX, PY}, [{useMask, true}])
+                          end, lists:reverse(Stamps)),
+            lists:foreach(fun({_Color, PX, PY}) ->
+                                  wxDC:drawBitmap(DC, RedBalloonBitmap, {PX, PY}, [{useMask, true}])
+                          end, lists:reverse(Balloons)),
+            wxBufferedPaintDC:destroy(DC),
+            loop(Frame, DartButton, FireButton, CanvasPanel, StartWaveButton, State);
 
         _Other ->
-            % !! THIS IS THE FIXED LINE !! It now correctly passes all 5 arguments.
-            loop(Frame, DartMonkeyWidget, FireMonkeyWidget, MainPID, State)
+            loop(Frame, DartButton, FireButton, CanvasPanel, StartWaveButton, State)
     end,
     wx:destroy().
