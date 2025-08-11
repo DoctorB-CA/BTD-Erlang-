cffde8b122671f4d1ac05a2faf71922d7b31ac2c
DoctorB <150272448+DoctorB-CA@users.noreply.github.com>
2025-08-11 12:05:02 +0300

    Update game.erl

--- game.erl
+++ game.erl
@@ -1,34 +1,28 @@
+%%% game.erl
+%%% Convenience: compile everything and start main_server (which starts regions + GUI).
+
 -module(game).
--export([start/0, compile_all/0]).
+-export([compile_all/0, start/0]).
 
 compile_all() ->
-    io:format("Compiling all game files...~n"),
+    io:format("Compiling...~n"),
     application:ensure_all_started(compiler),
     Modules = [w, region_server, bloon, arrow, monkey, main_server],
     lists:foreach(
-        fun(Module) ->
-            File = atom_to_list(Module) ++ ".erl",
-            case compile:file(File, [binary, report_errors, report_warnings]) of
-                {ok, CompiledModule, Bin} ->
-                    code:purge(CompiledModule),
-                    code:load_binary(CompiledModule, File, Bin),
-                    io:format("~-20s -> ok~n", [File]);
-                {error, Errors, Warnings} ->
-                    io:format("~-20s -> FAILED~n", [File]),
-                    io:format("Errors: ~p~nWarnings: ~p~n", [Errors, Warnings]),
-                    erlang:halt(1)
-            end
-        end,
-        Modules
-    ),
-    io:format("All modules compiled and loaded.~n").
+      fun(M) ->
+        File = atom_to_list(M) ++ ".erl",
+        case compile:file(File, [binary, report_errors, report_warnings]) of
+          {ok, Mod, Bin} ->
+              code:purge(Mod),
+              code:load_binary(Mod, File, Bin),
+              io:format("~-18s ok~n", [File]);
+          {error, E, W} ->
+              io:format("~-18s FAILED~nE=~p~nW=~p~n", [File, E, W]),
+              erlang:halt(1)
+        end
+      end, Modules),
+    io:format("All modules compiled.~n").
 
 start() ->
     compile_all(),
-    io:format("Starting the main server on workers...~n"),
-    main_server:start_link([
-        'worker1@119-lnx-20',
-        'worker2@119-lnx-20',
-        'worker3@119-lnx-20',
-        'worker4@119-lnx-20'
-    ]).
+    main_server:start_link().  %% uses your 4 workers and starts the GUI
