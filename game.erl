dceb7d9ce8264739a3596466cb176396b4288104
DoctorB <150272448+DoctorB-CA@users.noreply.github.com>
2025-08-11 14:21:19 +0300

    Update game.erl

--- game.erl
+++ game.erl
@@ -1,8 +1,6 @@
 %%% game.erl
-%%% Convenience: compile everything and start main_server (which starts regions + GUI).
-
 -module(game).
--export([compile_all/0, start/0]).
+-export([start/0, compile_all/0]).
 
 compile_all() ->
     io:format("Compiling...~n"),
@@ -10,19 +8,25 @@ compile_all() ->
     Modules = [w, region_server, bloon, arrow, monkey, main_server],
     lists:foreach(
       fun(M) ->
-        File = atom_to_list(M) ++ ".erl",
-        case compile:file(File, [binary, report_errors, report_warnings]) of
-          {ok, Mod, Bin} ->
-              code:purge(Mod),
-              code:load_binary(Mod, File, Bin),
-              io:format("~-18s ok~n", [File]);
-          {error, E, W} ->
-              io:format("~-18s FAILED~nE=~p~nW=~p~n", [File, E, W]),
-              erlang:halt(1)
-        end
+          File = atom_to_list(M) ++ ".erl",
+          case compile:file(File, [binary, report_errors, report_warnings]) of
+            {ok, Mod, Bin} ->
+                code:purge(Mod),
+                code:load_binary(Mod, File, Bin),
+                io:format("~-18s ok~n", [File]);
+            {error, E, W} ->
+                io:format("~-18s FAILED~nErrors: ~p~nWarnings: ~p~n", [File, E, W]),
+                erlang:halt(1)
+          end
       end, Modules),
     io:format("All modules compiled.~n").
 
 start() ->
     compile_all(),
-    main_server:start_link().  %% uses your 4 workers and starts the GUI
+    io:format("Main Server starting...~n"),
+    main_server:start_link([
+        'worker1@119-lnx-20',
+        'worker2@119-lnx-20',
+        'worker3@119-lnx-20',
+        'worker4@119-lnx-20'
+    ]).
