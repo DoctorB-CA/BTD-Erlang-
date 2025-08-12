5f38ec1a7f204064f3ba137af8c315ef052717e5
zombiman12 <noamnavon10@gmail.com>
2025-08-12 09:48:04 +0300

    Add files via upload

--- main_server.erl
+++ main_server.erl
@@ -1,59 +1,63 @@
 -module(main_server).
 -behaviour(gen_server).
-
 -export([start_link/1, add_monkey/2, add_bloon/2]).
--export([init/1, handle_call/3, handle_cast/2, handle_info/2]).
+-export([init/1, handle_call/3, handle_cast/2]).
 
--define(MAP_WIDTH, 200).
 -define(NUM_REGIONS, 4).
--define(REGION_WIDTH, ?MAP_WIDTH / ?NUM_REGIONS).
+-define(REGION_WIDTH, 50).
 
--record(state, { regions }). % regions is a list of {Node, Pid}
+% The state will now hold the actual PIDs of the remote regions.
+-record(state, { region_pids = [] }).
 
-start_link(RegionNodes) when is_list(RegionNodes) ->
-    gen_server:start_link({local, ?MODULE}, ?MODULE, [RegionNodes], []).
+start_link(AllNodes) -> gen_server:start_link({local, ?MODULE}, ?MODULE, [AllNodes], []).
+add_monkey(Pos, Range) -> gen_server:cast(?MODULE, {add_monkey, Pos, Range}).
+add_bloon(Path, Health) -> gen_server:cast(?MODULE, {add_bloon, Path, Health}).
 
-add_monkey(Pos, Range) ->
-    gen_server:cast(?MODULE, {add_monkey, Pos, Range}).
+init([AllNodes]) ->
+    io:format("Main Server started. Waiting for all regions to report in...~n"),
+    
+    RegionNameNodes = lists:zipwith(
+        fun(Id, Node) -> {list_to_atom("region_" ++ integer_to_list(Id)), Node} end,
+        lists:seq(0, ?NUM_REGIONS - 1),
+        AllNodes
+    ),
 
-add_bloon(Path, Health) ->
-    gen_server:cast(?MODULE, {add_bloon, Path, Health}).
+    % Use our new helper function to reliably get the PIDs, retrying a few times.
+    RegionPids = [get_remote_pid(NameNode, 5) || NameNode <- RegionNameNodes],
 
-init([RegionNodes]) ->
-    io:format("Main Server is starting...~n"),
-    ets:new(game_data, [set, public, named_table]),
-    RegionArgs = lists:map(fun(I) ->
-        {I, I * ?REGION_WIDTH, I * ?REGION_WIDTH + ?REGION_WIDTH - 1}
-    end, lists:seq(0, ?NUM_REGIONS - 1)),
-    NodeArgs = lists:zip(RegionNodes, RegionArgs),
-    RegionInfo = lists:map(fun({Node, {Id, StartX, EndX}}) ->
-        io:format("Main Server: Requesting node ~p to start region ~p...~n", [Node, Id]),
-        case rpc:call(Node, region_server, start_remotely, [Id, StartX, EndX]) of
-            {ok, Pid} -> {Node, Pid};
-            Error -> erlang:error({failed_to_start_remote_node, {Node, Error}})
-        end
-    end, NodeArgs),
-    io:format("Main Server: All start requests sent.~n"),
-    {ok, #state{regions = RegionInfo}}.
+    io:format("Main Server: All regions are up and running with PIDs: ~p~n", [RegionPids]),
+    {ok, #state{region_pids = RegionPids}}.
+
+
+handle_cast({add_monkey, Pos = {X, _Y}, Range}, State = #state{region_pids = Pids}) ->
+    RegionIndex = trunc(X / ?REGION_WIDTH),
+    RegionPid = lists:nth(RegionIndex + 1, Pids),
+    io:format("~p: Routing 'add_monkey' to region PID ~p~n", [node(), RegionPid]),
+    case is_pid(RegionPid) of
+        true -> gen_server:cast(RegionPid, {spawn_monkey, Pos, Range});
+        false -> io:format("~p: ERROR - Invalid PID for region ~p~n", [node(), RegionIndex])
+    end,
+    {noreply, State};
+
+handle_cast({add_bloon, Path = [{X, _Y} | _], Health}, State = #state{region_pids = Pids}) ->
+    RegionIndex = trunc(X / ?REGION_WIDTH),
+    RegionPid = lists:nth(RegionIndex + 1, Pids),
+    % This is now much simpler and correct. We already have the PIDs.
+    gen_server:cast(RegionPid, {spawn_bloon, Path, Health, Pids}),
+    {noreply, State}.
 
 handle_call(_Request, _From, State) -> {reply, ok, State}.
 
-handle_cast({add_monkey, Pos = {X, _Y}, Range}, State) ->
-    RegionIndex = trunc(X / ?REGION_WIDTH),
-    {Node, RegionPid} = lists:nth(RegionIndex + 1, State#state.regions),
-    io:format("Main Server: Requesting node ~p to start a monkey...~n", [Node]),
-    rpc:call(Node, monkey, start_remotely, [Pos, Range, RegionPid]),
-    {noreply, State};
 
-handle_cast({add_bloon, Path = [{X, _Y} | _], Health}, State) ->
-    RegionIndex = trunc(X / ?REGION_WIDTH),
-    % FIX: Get the Node for the bloon's starting region
-    {Node, RegionPid} = lists:nth(RegionIndex + 1, State#state.regions),
-    % Get a list of all region PIDs for the bloon to know about
-    AllRegionPids = [Pid || {_N, Pid} <- State#state.regions],
-    % FIX: Use rpc to start the bloon on the correct remote node
-    io:format("Main Server: Requesting node ~p to start a bloon...~n", [Node]),
-    rpc:call(Node, bloon, start_remotely, [Path, Health, RegionPid, AllRegionPids]),
-    {noreply, State}.
-
-handle_info(_Info, State) -> {noreply, State}.
+%% --- HELPER FUNCTION ---
+get_remote_pid(_NameNode, 0) ->
+    erlang:error({could_not_find_remote_pid, _NameNode});
+get_remote_pid({Name, Node} = NameNode, Retries) ->
+    case rpc:call(Node, erlang, whereis, [Name]) of
+        undefined ->
+            io:format("Region ~p not up yet, waiting...~n", [Name]),
+            timer:sleep(200),
+            get_remote_pid(NameNode, Retries - 1);
+        Pid when is_pid(Pid) ->
+            Pid
+    end.
