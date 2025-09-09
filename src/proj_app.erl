4649cd47abf358dd14b69a4857d5423d5eac99e8
CSE Student <csestudent@119-lnx-17.campus.ad.bgu.ac.il>
2025-09-09 05:11:01 +0300

    feat: Actively connect worker to main node on startup

--- src/proj_app.erl
+++ src/proj_app.erl
@@ -12,12 +12,20 @@ start(_StartType, _StartArgs) ->
     io:format("Starting 'proj' application...~n"),
     % Decide which supervisor to start based on the node's name.
     case is_main_node(node()) of
-        true ->
-            io:format("Node ~p is the main node. Starting main_supervisor.~n", [node()]),
-            main_supervisor:start_link();
-        false ->
-            io:format("Node '~p' is a worker node. Starting worker_root_supervisor.~n", [node()]),
-            worker_root_supervisor:start_link()
+        ?MAIN_NODE ->
+            io:format("Node '~p' is the main node. Starting main_supervisor.~n", [node()]),
+            proj:start_main_supervisor();
+        _WorkerNode ->
+            io:format("Node '~p' is a worker node. Trying to connect to main node...~n", [node()]),
+            case net_kernel:connect_node(?MAIN_NODE) of
+                true ->
+                    io:format("Successfully connected to main node. Starting worker_root_supervisor.~n"),
+                    proj:start_worker_root_supervisor();
+                false ->
+                    io:format("Error: Could not connect to main node ~p. Aborting.~n", [?MAIN_NODE]),
+                    {error, could_not_connect_to_main_node}
+            end
+    end.
     end.
 
 stop(_State) ->
