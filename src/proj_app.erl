2048d83cd8318c8e2b97cf87c66bfb591c07bb1f
CSE Student <csestudent@119-lnx-17.campus.ad.bgu.ac.il>
2025-09-09 05:18:54 +0300

    fix: call start_link directly instead of non-existent wrapper

--- src/proj_app.erl
+++ src/proj_app.erl
@@ -16,13 +16,13 @@ start(_StartType, _StartArgs) ->
     case node() of
         ?MAIN_NODE ->
             io:format("Node '~p' is the main node. Starting main_supervisor.~n", [node()]),
-            proj:start_main_supervisor();
+            main_supervisor:start_link();
         _WorkerNode ->
             io:format("Node '~p' is a worker node. Trying to connect to main node...~n", [node()]),
             case net_kernel:connect_node(?MAIN_NODE) of
                 true ->
                     io:format("Successfully connected to main node. Starting worker_root_supervisor.~n"),
-                    proj:start_worker_root_supervisor();
+                    worker_root_supervisor:start_link();
                 false ->
                     io:format("Error: Could not connect to main node ~p. Aborting.~n", [?MAIN_NODE]),
                     {error, could_not_connect_to_main_node}
